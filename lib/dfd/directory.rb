ac532413bf5adca3b32215d4d4a79ca38f872cec
Finntaur <finntaur@gmail.com>
2020-06-04 21:42:52 +0300

    Ignore symbolic links when indexing files

--- lib/dfd/directory.rb
+++ lib/dfd/directory.rb
@@ -8,6 +8,7 @@ module DFD
 
     def add(path, recursive:, include_all:)
       absolute_path = File.absolute_path(path)
+      return if File.symlink?(absolute_path)
       if File.file?(absolute_path)
         @files << absolute_path
       elsif File.directory?(absolute_path) and recursive
